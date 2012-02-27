6ec7f73cf8a6eec70122a5f12abb4a550d733bf2
Dieter Verfaillie <dieterv@optionexplicit.be>
2012-02-27 23:41:57 +0100

    Add copyright blurbs

--- lib/pygtk2exe/command/build_exe.py
+++ lib/pygtk2exe/command/build_exe.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
 
+# Copyright © 2010-2012 pygtk2exe Contributors
+#
+# This file is part of pygtk2exe.
+#
+# pygtk2exe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pygtk2exe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pygtk2exe. If not, see <http://www.gnu.org/licenses/>.
+
 
 import os
 
